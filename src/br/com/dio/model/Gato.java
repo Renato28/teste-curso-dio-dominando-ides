9dd39a7ab1d77df388c88eecbed8a8088393063d
Renato Nobrega <renatonobrega87@gmail.com>
2022-05-11 22:16:54 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,47 @@
+package br.com.dio.model;
+
+public class Gato {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+	
+	public Gato(String nome, String cor, Integer idade) {
+		
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Integer getIdade() {
+		return idade;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+}
---
+++ src/br/com/dio/model/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio.model;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("Foguinho", "Amarelo", 1);
+		
+	}
+}
